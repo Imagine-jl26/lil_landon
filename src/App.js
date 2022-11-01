e977019ce4c40a74ed69a05eed503511cdc7e3ce
Maxim Karnaukh <maxim_karnaukh@epam.com>
2022-11-01 16:59:14 +0600

    Testing of the AWS Delivery

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Testing of the AWS Delivery
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
